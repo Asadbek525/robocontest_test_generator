b68668811ca763168ed69f521a9bb3dce54e4838
devasadbek <devasadbek@gmail.com>
2023-04-11 14:59:20 +0500

    ready for use

--- main.py
+++ main.py
@@ -14,4 +14,5 @@ shutil.copyfile("template.py", nameOfProblem + "/" + "_py.py")
 shutil.copyfile("checker.py", nameOfProblem + "/" + "_checker.py")
 shutil.copyfile("template.cpp", nameOfProblem + "/" + "_cpp" + ".cpp")
 shutil.copyfile("checker.cpp", nameOfProblem + "/" + "_checker" + ".cpp")
+shutil.copyfile("zipper.py", nameOfProblem + "/" + "_zipper.py")
 
--- zipper.py
+++ zipper.py
@@ -2,13 +2,34 @@ import os
 import shutil
 import sys
 
-folder_name = sys.argv[1]
 
-def zip_folder(folder_name):
-    shutil.make_archive(folder_name, 'zip', folder_name)
+def zip_test_cases():
+    # zip all .in and .out files in the current directory
+    # and put them in a folder called test_cases
+    # and then zip the folder
+    # and then delete the folder
+
+    # create the folder
+    try:
+        os.mkdir("test_cases")
+    except OSError:
+        print("Creation of the directory %s failed" % "test_cases")
+
+    # copy all .in and .out files to the folder
+    for file in os.listdir():
+        if file.endswith(".in") or file.endswith(".out"):
+            shutil.copyfile(file, "test_cases/" + file)
+
+    # zip the folder
+    shutil.make_archive("_test_cases", "zip", "test_cases")
+
+    # delete the folder
+    shutil.rmtree("test_cases")
+
 
 def main():
-    zip_folder(folder_name)
+    zip_test_cases()
+
 
 if __name__ == '__main__':
     main()
