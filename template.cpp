822c7191a7f0cb0040a34bf2755431850ad3cfd9
Asadbek <68222862+Asadbek525@users.noreply.github.com>
2023-06-04 20:35:51 +0500

    c++ import improved

--- template.cpp
+++ template.cpp
@@ -1,6 +1,18 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <algorithm>
+
 using namespace std;
 
+#define int long long
+
+int randint(int start, int end) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> dis(start, end);
+    return dis(gen);
+}
 
 string to_4digit(int n) {
     string s = to_string(n);
@@ -10,25 +22,25 @@ string to_4digit(int n) {
     return s;
 }
 
-int main() {
+signed main() {
 
     // open tests folder
-    int k = system("mkdir tests");
-    if (k == -1) {
-        cout << "tests folder already exists";
-    }
-    int start = 1;
-    int end = 100;
-    for (int i = start; i <= end; i++) {
-        int n = rand() % 1000 + 1;
+    system("mkdir tests");
+    // A + B example
+    int from = 1;
+    int to = 100;
+    for (int i = from; i <= to; i++) {
+        int A = randint(1, 1e9);
+        int B = randint(1, 1e9);
+
         ofstream f;
         f.open("tests/" + to_4digit(i) + ".in");
         // input here
-        f << n;
+        f << A << " " << B << endl;
         f.close();
         f.open("tests/" +  to_4digit(i) + ".out");
         // output here
-        f << "";
+        f << A + B << endl;
         f.close();
     }
 }
