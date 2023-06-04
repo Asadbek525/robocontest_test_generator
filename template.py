3dc7678f827e4033304cbb968bab6c79d74d6767
Asadbek <68222862+Asadbek525@users.noreply.github.com>
2023-06-04 20:37:31 +0500

    Update template.py

--- template.py
+++ template.py
@@ -8,13 +8,14 @@ except FileExistsError:
     pass
 
 
-start = 1
-end = 100
-for i in range(start, end + 1):
-    n = randint(1, 1000)
+from = 1
+to = 100
+for i in range(from, to + 1):
+    A = randint(1, int(1e9))
+    B = randint(1, int(1e9))
     with open(f'tests/{i:04}.in', 'w') as f:
         # input here
-        f.write(f'{n}')
+        f.write(f'{A} {B}')
     with open(f'tests/{i:04}.out', 'w') as f:
         # output here
-        f.write(f'')
+        f.write(f'{A + B}')
