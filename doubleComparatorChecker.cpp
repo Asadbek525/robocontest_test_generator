ea70af931a8ca71d2972bf7b127c13a83e6fc8e1
Asadbek <68222862+Asadbek525@users.noreply.github.com>
2023-06-05 16:36:49 +0500

    Update doubleComparatorChecker.cpp

--- doubleComparatorChecker.cpp
+++ doubleComparatorChecker.cpp
@@ -19,7 +19,7 @@ ifstream output("author.txt");
 ifstream user_output("user.txt");
 ifstream code("code.txt");
 
-void close(int x) {
+void ret(int x) {
     input.close();
     output.close();
     user_output.close();
@@ -41,17 +41,17 @@ int main()
         user_output >> user_ans;
         string s;
         if (user_output >> s) {
-            close(pe);
+            ret(pe);
         }
 
         if (abs(ans - user_ans) <= max_error) {
-            close(ac);
+            ret(ac);
         }
         else {
-            close(wa);
+            ret(wa);
         }
     }
-    catch {
-        close(pe);
+    catch (const std::exception& e) {
+        ret(pe);
     }
 }
