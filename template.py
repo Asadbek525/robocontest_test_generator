d3b39f92ddde5cbb52557505e5b552669e1d4d74
Asadbek <68222862+Asadbek525@users.noreply.github.com>
2023-06-05 15:40:05 +0500

    Update template.py

--- template.py
+++ template.py
@@ -8,9 +8,9 @@ except FileExistsError:
     pass
 
 
-from = 1
-to = 100
-for i in range(from, to + 1):
+start = 1
+end = 100
+for i in range(start, end + 1):
     A = randint(1, int(1e9))
     B = randint(1, int(1e9))
     with open(f'tests/{i:04}.in', 'w') as f:
